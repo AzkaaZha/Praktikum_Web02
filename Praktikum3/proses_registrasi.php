58b99a2093d692d1e4917b865e1fe8e85b97467b
AzkaZharari <azka22060ti@student.nurulfikri.ac.id>
2023-03-07 15:38:48 +0700

    pertemuan 3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+$domisili  = ['Jakarta', 'Bekasi', 'Depok', 'Tangerang', 'Bogor'];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
+
+$skill = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "RWD Boostrap" => 20,
+    "Python" => 30,
+    "PHP" => 30,
+    "Java" => 50
+];
+
+?>
