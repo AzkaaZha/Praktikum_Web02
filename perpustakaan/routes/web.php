f52414f95b4deeff79b2c5b6f5c83b3d6d2f8ff9
AzkaZharari <azka22060ti@student.nurulfikri.ac.id>
2023-06-20 14:54:33 +0700

    Praktikum 13

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DaftarBukuController;
 use App\Http\Controllers\DaftarAnggotaController;
 use App\Http\Controllers\FormBukuController;
-
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,49 +55,53 @@ Route::get('/kabar', function () {
 
    //Praktikum 10
    Route::get('/dashboard', 
-   [DashboardController::class, 'index']);
+   [DashboardController::class, 'index'])->middleware('auth');
 
    Route::get('book/daftar_buku', 
-   [DaftarBukuController::class, 'index']);
+   [DaftarBukuController::class, 'index'])->middleware('auth');
 
    Route::get('member/daftar_anggota', 
-   [DaftarAnggotaController::class, 'index']);
+   [DaftarAnggotaController::class, 'index'])->middleware('auth');
 
    //Praktikum 12
    Route::get('book/create', 
-   [DaftarBukuController::class, 'create']);
+   [DaftarBukuController::class, 'create'])->middleware('auth');
 
    Route::post('book/store', 
-   [DaftarBukuController::class, 'store']);
+   [DaftarBukuController::class, 'store'])->middleware('auth');
 
    Route::delete('book/destroy/{id}', 
-   [DaftarBukuController::class, 'destroy']);
+   [DaftarBukuController::class, 'destroy'])->middleware('auth');
 
    Route::get('member/create', 
-   [DaftarAnggotaController::class, 'create']);
+   [DaftarAnggotaController::class, 'create'])->middleware('auth');
 
    Route::post('member/store', 
-   [DaftarAnggotaController::class, 'store']);
+   [DaftarAnggotaController::class, 'store'])->middleware('auth');
 
    Route::delete('member/destroy/{id}', 
-   [DaftarAnggotaController::class, 'destroy']);
+   [DaftarAnggotaController::class, 'destroy'])->middleware('auth');
 
    //Praktikum 13
    Route::get('book/edit/{id}', 
-   [DaftarBukuController::class, 'edit']);
+   [DaftarBukuController::class, 'edit'])->middleware('auth');
 
    Route::put('book/update/{id}', 
-   [DaftarBukuController::class, 'update']);
+   [DaftarBukuController::class, 'update'])->middleware('auth');
 
    Route::get('book/show/{id}', 
-   [DaftarBukuController::class, 'show']);
+   [DaftarBukuController::class, 'show'])->middleware('auth');
 
    Route::get('member/edit/{id}', 
-   [DaftarAnggotaController::class, 'edit']);
+   [DaftarAnggotaController::class, 'edit'])->middleware('auth');
 
    Route::put('member/update/{id}', 
-   [DaftarAnggotaController::class, 'update']);
+   [DaftarAnggotaController::class, 'update'])->middleware('auth');
 
    Route::get('member/show/{id}', 
-   [DaftarAnggotaController::class, 'show']);
+   [DaftarAnggotaController::class, 'show'])->middleware('auth');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
